9d9eb9d1804e62170b61124ddac013f64f2fd220
mkostinuk <mkostinuk@gmail.com>
2024-09-22 01:24:18 +0300

    init

---
+++ src/main/java/org/example/fido2/Fido2Application.java
@@ -0,0 +1,13 @@
+package org.example.fido2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fido2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fido2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/fido2/Fido2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fido2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fido2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
